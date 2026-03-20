8acf5d3ec14a1a251a7c758d618fb98f76b1eaf4
Ying Wang <ying@paintcreeksoftware.com>
2026-03-20 20:41:07 +0000

    Update config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
-import nextMDX from '@next/mdx'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  outputFileTracingIncludes: {
-    '/articles/*': ['./src/app/articles/**/*.mdx'],
-  },
+  output: 'export',
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: ['remark-gfm'],
-    rehypePlugins: ['@mapbox/rehype-prism'],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
